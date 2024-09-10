e05f60d39575df078210236bf8b38b2c29cf2a91
Serg18353 <mikholenok@yandex.ru>
2024-09-10 20:34:25 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,8 @@
+name = 'Sergey'
+Age = '50'
+New_Age = str(int(Age)+2)
+Is_Student = True
+print('Name: '+ name)
+print('Age: ' + Age)
+print('New Age: ' + New_Age)
+print ('Is Student: ' + str(Is_Student))
